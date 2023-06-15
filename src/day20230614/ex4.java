54ae8f2fc0f3c2112954cc161008d4eb23393e5a
NamKiHyeok <carpediem7490@gmail.com>
2023-06-15 09:20:03 +0900

    숙제 수정

--- src/day20230614/ex4.java
+++ src/day20230614/ex4.java
@@ -7,12 +7,30 @@ public class ex4 {
 		// 소인수 분해 : 어떤 수를 소수인 인수로 분해하는 것.
 		// 못풀었다
 		
-		int x = 66;
-		for (int i = 0, j = 2; i < 66; i++, j++) {
-			while(x % j == 0) {
-				x /= j;
-				System.out.printf("%d ", j);
-			}
-		}
+//		풀이1)
+		
+//		int x = 66;
+//		for (int i = 0, j = 2; i < 66; i++, j++) {
+//			while(x % j == 0) {
+//				x /= j;
+//				System.out.printf("%d ", j);
+//			}
+//		}
+		
+//		풀이2)
+//		// 1은 소수가 아니므로 i의 기준을 2로 잡음.
+//		for (int i = 2; i <= primeFactorization; i++) {
+//
+//			// 66을 i(2)로 나누면 나머지 0, true값이니 while문 실행
+//			while ((primeFactorization % i) == 0) {
+//		    
+//				// 2는 소수이면서 합성수 66을 구성하는 소수이므로 2출력
+//				System.out.println(i);
+//		        
+//				//66을 2로 나눠준 값 33을 primeFactorization에 저장.
+//				primeFactorization = primeFactorization / i;
+//			}
+//
+//		}
 	}
 }
--- src/day20230614/ex6.java
+++ src/day20230614/ex6.java
@@ -4,7 +4,7 @@ public class ex6 {
 
 	public static void main(String[] args) {
 		// 1 1 2 3 5 8 13 21 34 55를 출력하는 코드를 작성하시오. int x = 1; int y = 0, int z = 0;
-		// 못풀었다
+		// hint 출력코드 맨 앞에 0이 있다고 가정한다.
 		int x = 1;
 		int y = 0;
 		int z = 0;
