05112b13b4acaede95230b134a11d1310378b524
NamKiHyeok <carpediem7490@gmail.com>
2023-06-16 17:34:55 +0900

    2023.06.16 숙제수정

--- src/day20230616/OmokProgram.java
+++ src/day20230616/OmokProgram.java
@@ -3,30 +3,25 @@ package day20230616;
 public class OmokProgram {
 
 	public static void main(String[] args) {
-		for(int j=0; j<12; j++) {
-			for(int i=0; i<12; i++)
-				// 첫째 줄
-				if(j==0&&i==0)
-					System.out.print("┌");
-				else if(j==0&&i==11)
-					System.out.print("┐");
-				else if(j==11&&i==0)
-					System.out.print("└");
-				else if(j==11&&i==11)
-					System.out.print("┘");
-				else if(j==0)
-					System.out.print("┬");
-				else if(j==11)
-					System.out.print("┴");
-				else if(i==0)
-					System.out.print("├");
-				else if(i==11)
-					System.out.print("┤");
-				else 
-					System.out.print("┼");
-			
-			
-		}
+//		전략 모든 경우의수를 구해주고 특수한 경우를 가장 먼저 보편적인 경우를 가장 나중에 세워준다.
+		for(int j=0;j<10;j++) { 
+		    for(int i=0;i<10;i++) {
+		    	if(j==0&&i==0)
+		    		System.out.print("┌");
+		    	else if(j==0&&i==9)
+		    		System.out.print("┐");
+		    	else if(j==9&&i==0)
+		    		System.out.print("└");
+		    	else if(j==9&&i==9)
+		    		System.out.print("┘");
+		    	else if(j==0)
+		    		System.out.print("┬");
+		    	else if(j==9)
+		    		System.out.print("┴");
+		    	else
+		    		System.out.print("┼");
+		   }
+		    System.out.println();
+		}		
 	}
-
 }
