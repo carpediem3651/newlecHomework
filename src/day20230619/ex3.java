18493a52010f13fca7dd279c70df188537d5542f
NamKiHyeok <carpediem7490@gmail.com>
2023-06-19 17:26:57 +0900

    2023.06.19 숙제수정

--- src/day20230619/ex3.java
+++ src/day20230619/ex3.java
@@ -4,7 +4,7 @@ public class ex3 {
 
 	public static void main(String[] args) {
 		for(int i=0; i<5; i++) {
-			for(int j=0; j<4-i; j++) {	//공백을 담당하는 for문
+			for(int j=0; j<4-i; j++) {	//공백을 담당하는 for문 이게 먼저 출력된다. 즉 공백이 먼저 출력.
 				System.out.print(" ");
 			}
 			for(int j=0; j<i+1; j++) {	//별을 담당하는 for문
--- src/day20230619/ex5.java
+++ src/day20230619/ex5.java
@@ -3,14 +3,14 @@ package day20230619;
 public class ex5 {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=5; i++) {
-			for(int j=1; j<=5-i; j++) {
-				System.out.print(" ");
+		for(int i=0;i<5;i++){
+			for(int j=0;j<4-i;j++){
+				System.out.print(" "); 
 			}
-			for(int j=1; j<=(i*2)-1; j++) {
-				System.out.print("*");
+			for(int j=0;j<2*i+1;j++){
+				System.out.print("*"); 
 			}
-			System.out.println();		
-		}
+			System.out.println("");
+		} 
 	}
 }
