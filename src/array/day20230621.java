f5b9e0800d9f274c6ac6e48d3b9f3b493ef34e13
NamKiHyeok <carpediem7490@gmail.com>
2023-06-21 18:30:20 +0900

    2023.06.21 숙제수정

---
+++ src/array/day20230621.java
@@ -0,0 +1,114 @@
+package array;
+
+import java.util.Scanner;
+
+public class day20230621 {
+
+	public static void main(String[] args) {
+//		문제1.정수, 실수, 문자, boolean 배열로 표현하시오.
+//		byte[] x = new byte[5];
+//		short[] shortArray = new short[5];
+//		int[] x = new int[5];
+//		long[] x = new long[5];
+//		float[] y = new float[5];
+//		double[] z = new double[5];
+//		char[] l = new char[5];
+//		boolean[] m = new boolean[5];
+		
+//		문제2.int형 배열 numbers를 선언하세요.
+//		int[] numbers = new int[1];
+		
+//		문제3.int형 배열 numbers는 기본타입인가요?
+//		문제4.int형 배열 numbers에 크기 10인 배열을 만든 후, 할당하세요
+//		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+		
+//		문제5.int형 배열 numbers의 2번째에 숫자 3을 할당하세요.
+//		int[] numbers = new int[10];
+//		numbers[1] = 3;
+		
+//		문제6. long형 배열 longNumbers에 크기 3인 배열을 만든 후, 할당하세요
+//		long[] longNumbers = new long[3];
+//		longNumbers[0] = 1;
+//		longNumbers[1] = 2;
+//		longNumbers[2] = 3;
+		
+////		문제7. long형 배열 longNumbers에 크기 3인 배열을 만든 후, index 2번째를 출력해보세요.
+//		long[] longNumbers = {1,2,3};
+//		System.out.println(longNumbers[2]);
+		
+//		문제8. boolean형 배열 isChecked를 선언하세요. isChecked에 true, false, true를 new 연산자없이 할당하세요.
+//		boolean[] isChecked = {true, false, true};
+		
+//		문제9. 크기가 20인 int형 배열 numbers를 선언하세요. numbers의 index 0부터 19까지 for문을 이용해서 0~19를 할당해주세요
+		
+//		int[] numbers = new int[20];
+//		for(int i=0; i<20; i++) {
+//			numbers[i] = i;
+//			System.out.println(numbers[i]);
+//		}
+		
+//		문제10. 크기가 10인 문자형 배열 chars를 선언하세요. 반복문을 이용하여 chars의 index가 3의 배수일 땐 'a'를, 5의 배수일 때
+//		'b'를 할당하여 출력해보세요.
+		
+//		char[] chars = new char[10]; // 통이 만들어졌다.
+//		for(int i=0; i<10; i++) {
+//			if(i%3 == 0 && i!=0) {
+//				chars[i] = 'a';
+//			} else if(i%5 == 0 && i!=0){
+//				chars[i]='b';
+//			} else 
+//				chars[i]='x';
+//			System.out.println(chars[i]);
+//		}
+		
+////		문제11. 5,7,3의 출력 순서를 3,5,7로 바꿔쓰시오.
+//		int a = 5;
+//		int b = 7;
+//		int c = 3;
+//		
+//		int tmp;
+//		tmp = a;
+//		a = c;
+//		c = b;
+//		b = tmp;
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+		
+////		문제12. 1을 입력하면, 계속 메뉴를 선택하고 9를 누르면 종료가 되는 프로그램을 만들어보세요.
+//		GAMEOVER:
+//		while (true) {
+//			System.out.println("메뉴를 선택하시오");
+//			System.out.println("1.계속"+" "+"9.종료");
+//			System.out.println(">입력값");
+//
+//
+//			Scanner scan = new Scanner(System.in);
+//			int menu = scan.nextInt(); 
+//
+//			switch (menu) {
+//			case 1: {
+//				System.out.println("메뉴를 선택하시오");
+//				System.out.println("1.계속"+" "+"9.종료");
+//				System.out.println(">입력값");
+//				
+//				menu = scan.nextInt();
+//				
+//				break;
+//			} // case 1
+//			
+//			case 9: {
+//				System.out.println("종료되었습니다.");
+//				break GAMEOVER;
+//
+//			}
+//			default:
+//				System.out.println("메뉴는 1과 9밖에 없습니다. 다른 값을 선택해주세요.");
+//				
+//			}// switch
+//		} // while
+		
+	}
+
+}
